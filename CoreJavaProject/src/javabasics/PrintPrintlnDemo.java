67add97855ef5d6a553a2cb11df1e43ae593e791
bpoongs <bpoongs@gmail.com>
2024-03-25 20:02:02 +0530

    operators

---
+++ CoreJavaProject/src/javabasics/PrintPrintlnDemo.java
@@ -0,0 +1,24 @@
+package javabasics;
+
+public class PrintPrintlnDemo {
+
+	public static void main(String[] args) {
+		// ln means line
+		// System.out.println() - after printing the cursor moves to the new line
+		// System.out.print() - after printing the cursor doen not move to the new line
+		
+		System.out.print("Hello, How are you?");
+		System.out.print("I am good. Thankyou. How about you?");
+		System.out.print("I am fine.");
+		
+		//System.out.println();
+		System.out.println("----------------------");
+		System.out.println("Hello, How are you?");
+		System.out.println("I am good. Thankyou. How about you?");
+		System.out.println("I am fine.");
+
+		System.out.println("----------------------");
+		// Do we have System.out.printf() in java?
+	}
+
+}
