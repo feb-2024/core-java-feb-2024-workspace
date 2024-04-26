212738430a01b97723ef57725fe9b979a8af823a
bpoongs <bpoongs@gmail.com>
2024-04-26 19:47:24 +0530

    StringBuffer StringBuilder

---
+++ CoreJavaProject/src/strings/StringBuilderDemo.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class StringBuilderDemo {
+
+	public static void main(String[] args) {
+		StringBuilder strBuild = new StringBuilder();
+		strBuild.append("How ");
+		strBuild.append("are ");
+		strBuild.append("you?");
+		
+		System.out.println(strBuild);
+		
+	}
+
+}
