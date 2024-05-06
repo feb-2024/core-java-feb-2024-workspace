eec5d9a7d0df43f03573fba1ea2e7bc16dfc8a3d
bpoongs <bpoongs@gmail.com>
2024-05-06 19:56:13 +0530

    generics and collections

---
+++ CoreJavaProject/src/generics/BoxGenerics.java
@@ -0,0 +1,44 @@
+package generics;
+//BoxGenerics class with generics
+public class BoxGenerics<T> {
+	private T length;
+	private T width;
+	private T height;
+	
+	public BoxGenerics(T length, T width, T height) {
+		this.length = length;
+		this.width = width;
+		this.height = height;
+	}
+
+	public T getLength() {
+		return length;
+	}
+
+	public void setLength(T length) {
+		this.length = length;
+	}
+
+	public T getWidth() {
+		return width;
+	}
+
+	public void setWidth(T width) {
+		this.width = width;
+	}
+
+	public T getHeight() {
+		return height;
+	}
+
+	public void setHeight(T height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "BoxGenerics [length=" + length + ", width=" + width + ", height=" + height + "]";
+	}
+	
+	
+}
